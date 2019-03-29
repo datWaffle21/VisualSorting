0b817f87e9d5ab48f74a4db7823c37365d417ddb
Noah <noah.wimmer10@gmail.com>
2019-03-28 20:07:09 -0500

    added more sorts

--- src/core/main/Main.java
+++ src/core/main/Main.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 public class Main extends Canvas implements Runnable {
@@ -14,7 +16,11 @@ public class Main extends Canvas implements Runnable {
     private Thread thread;
 
     private int i = 0;
+    private int currentComp = 0;
     private boolean go = false;
+
+    private int totalComps = 0;
+    private int swaps = 0;
 
     private boolean running = false;
     public int[] nums = new int[1000];
@@ -22,8 +28,15 @@ public class Main extends Canvas implements Runnable {
 
     private void renderBars(Graphics g) {
         g.setColor(Color.WHITE);
+        //g.drawString(currentComp + "", 30, 100);
         for (int i = 0; i < nums.length; i++) {
-            g.drawRect(i, (Constants.HEIGHT - (nums[i] / 2)) - 40, Constants.WIDTH / nums.length, nums[i] / 2);
+            if (i != currentComp) {
+                g.setColor(Color.WHITE);
+                g.drawRect(i, (Constants.HEIGHT - (nums[i] / 2)) - 40, Constants.WIDTH / nums.length, nums[i] / 2);
+            } else {
+                g.setColor(Color.RED);
+                g.fillRect(i, (Constants.HEIGHT - (nums[i] / 2)) - 40, Constants.WIDTH / nums.length, nums[i] / 2);
+            }
         }
     }
 
@@ -55,15 +68,18 @@ public class Main extends Canvas implements Runnable {
         int smallestIndex = i;
         for (int j = i + 1; j < nums.length; j++) {
             // gets the smallest int
+            totalComps++;
             if (nums[j] < smallest) {
                 smallest = nums[j];
                 smallestIndex = j;
             }
         }
 
+        swaps++;
         int temp = nums[i];
         nums[i] = nums[smallestIndex];
         nums[smallestIndex] = temp;
+        currentComp = smallestIndex;
         i++;
     }
 
@@ -72,12 +88,15 @@ public class Main extends Canvas implements Runnable {
         for (int i = 0; i < nums.length - 1 - count; i++) {
             int a = nums[i];
             int b = nums[i + 1];
+            currentComp = i;
+            totalComps++;
             if (a > b) {
                 int temp = a;
                 a = b;
                 b = temp;
                 nums[i] = a;
                 nums[i + 1] = b;
+                swaps++;
             }
         }
         count++;
@@ -88,9 +107,12 @@ public class Main extends Canvas implements Runnable {
         if (go) {
             //bubble();
             //selection();
+            //insertionSort(nums);
+            //cocktailSort(nums);
             //quickSort(nums, 0, nums.length - 1);
-            mergeSort(nums, 0 ,nums.length -1);
-            sleep(10);
+            //mergeSort(nums, 0 ,nums.length -1);
+            //radixSort(nums);
+            //sleep(8);
         }
     }
 
@@ -113,6 +135,10 @@ public class Main extends Canvas implements Runnable {
             g.setColor(Color.RED);
             g.fillRect(30, 30, 32, 32);
         }
+
+        g.setColor(Color.WHITE);
+        g.drawString("Total Comparisions: " + totalComps, 30, 100);
+        g.drawString("Total Swaps: " + swaps, 30, 115);
 
         g.dispose();
         bs.show();
@@ -179,7 +205,7 @@ public class Main extends Canvas implements Runnable {
         if (low < high) {
             /* pi is partitioning index, arr[pi] is
               now at right place */
-            if(go) {
+            if (go) {
                 int pi = partition(arr, low, high);
 
                 // partition and after partition
@@ -196,6 +222,7 @@ public class Main extends Canvas implements Runnable {
         for (int j = low; j < high; j++) {
             // If current element is smaller than or
             // equal to pivot
+            totalComps++;
             if (arr[j] <= pivot) {
                 i++;
 
@@ -203,6 +230,7 @@ public class Main extends Canvas implements Runnable {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
+                swaps++;
                 render();
                 sleep(6);
             }
@@ -222,21 +250,20 @@ public class Main extends Canvas implements Runnable {
 
     }
 
-    void merge(int arr[], int l, int m, int r)
-    {
+    void merge(int[] arr, int l, int m, int r) {
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
         /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
+        int[] L = new int[n1];
+        int[] R = new int[n2];
 
         /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
+        for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
 
         /* Merge the temp arrays */
@@ -246,34 +273,31 @@ public class Main extends Canvas implements Runnable {
 
         // Initial index of merged subarry array
         int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
+        while (i < n1 && j < n2) {
+            totalComps++;
+            if (L[i] <= R[j]) {
+                swaps++;
                 arr[k] = L[i];
                 i++;
-            }
-            else
-            {
+            } else {
+                swaps++;
                 arr[k] = R[j];
                 j++;
             }
             k++;
-            sleep(1);
+            //sleep(1);
             render();
         }
 
         /* Copy remaining elements of L[] if any */
-        while (i < n1)
-        {
+        while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
 
         /* Copy remaining elements of R[] if any */
-        while (j < n2)
-        {
+        while (j < n2) {
             arr[k] = R[j];
             j++;
             k++;
@@ -281,19 +305,135 @@ public class Main extends Canvas implements Runnable {
         render();
     }
 
-    void mergeSort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
+    void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
             // Find the middle point
-            int m = (l+r)/2;
+            int m = (l + r) / 2;
 
             // Sort first and second halves
             mergeSort(arr, l, m);
-            mergeSort(arr , m+1, r);
+            mergeSort(arr, m + 1, r);
 
             // Merge the sorted halves
             merge(arr, l, m, r);
         }
+    }
+
+    void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            //render();
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                render();
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    void cocktailSort(int[] a) {
+        boolean swapped = true;
+        int start = 0;
+        int end = a.length;
+
+        while (swapped == true) {
+            // reset the swapped flag on entering the
+            // loop, because it might be true from a
+            // previous iteration.
+            swapped = false;
+
+            // loop from bottom to top same as
+            // the bubble sort
+            for (int i = start; i < end - 1; ++i) {
+                totalComps++;
+                if (a[i] > a[i + 1]) {
+                    swaps++;
+                    render();
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // if nothing moved, then array is sorted.
+            if (swapped == false)
+                break;
+
+            // otherwise, reset the swapped flag so that it
+            // can be used in the next stage
+            swapped = false;
+
+            // move the end point back by one, because
+            // item at the end is in its rightful spot
+            end = end - 1;
+
+            // from top to bottom, doing the
+            // same comparison as in the previous stage
+            for (int i = end - 1; i >= start; i--) {
+                totalComps++;
+                if (a[i] > a[i + 1]) {
+                    swaps++;
+                    render();
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // increase the starting point, because
+            // the last stage would have moved the next
+            // smallest number to its rightful spot.
+            start = start + 1;
+        }
+    }
+
+    public void radixSort(int[] arr) {
+        Queue<Integer>[] buckets = new Queue[10];
+        for (int i = 0; i < 10; i++)
+            buckets[i] = new LinkedList<Integer>();
+
+        boolean sorted = false;
+        int expo = 1;
+
+        while (!sorted) {
+            sorted = true;
+            //render();
+            for (int item : arr) {
+                render();
+                int bucket = (item / expo) % 10;
+                if (bucket > 0) sorted = false;
+                buckets[bucket].add(item);
+            }
+
+            expo *= 10;
+            int index = 0;
+
+            for (Queue<Integer> bucket : buckets) {
+                while (!bucket.isEmpty()) {
+                    render();
+                    arr[index++] = bucket.remove();
+                }
+            }
+
+        }
+
+        assert isSorted(arr);
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i - 1] > arr[i])
+                return false;
+
+        return true;
     }
 }
